2637b52009a908bcc1a4b9da1493a33522a3251b
oybekjon94 <x.oybek94@gmail.com>
2023-05-10 23:32:18 +0900

    [feat::] Add Example01

---
+++ src/main/kotlin/Example01/03.kt
@@ -0,0 +1,18 @@
+package Example01
+
+fun main(){
+    var fullName:String = "Don Felker"
+    println(fullName.length)
+    println(fullName.decapitalize())
+    println(fullName.toLowerCase())
+
+    val firstName = "Donn"
+
+    val c:Char = 'x' //16-bit Unicode Character
+
+    val foo:String = "y"
+    println(firstName)
+    println(c)
+    println(foo)
+
+}
