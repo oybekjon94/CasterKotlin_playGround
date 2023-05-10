a869bbefca2cd0127d7a41ddfb5d9648a508ba30
oybekjon94 <x.oybek94@gmail.com>
2023-05-10 23:05:11 +0900

    [feat::] Add Example01

---
+++ src/main/kotlin/Example01/01.kt
@@ -0,0 +1,17 @@
+package Example01
+
+fun main(){
+    println("Hello World!")
+    var fullName = "Donn Felker"
+    println(fullName)
+
+    fullName = "John Felker"
+    println(fullName)
+
+    fullName = ""
+    println(fullName.isEmpty())
+
+    var age = 30
+    age = 32
+    println(age)
+}
