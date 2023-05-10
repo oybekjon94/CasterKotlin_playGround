7bd477d13bc4b5e5da8927a811b17e2173ddcf92
oybekjon94 <x.oybek94@gmail.com>
2023-05-11 06:06:31 +0900

    [feat::] Add Example01

---
+++ src/main/kotlin/Example01/09.kt
@@ -0,0 +1,15 @@
+package Example01
+
+fun main(){
+    val myAge = 32
+    val yourAge = 32
+
+    val myName = "Donn"
+    val yourName = "Tushar"
+
+    if ((myAge != yourAge)||(myName != yourName)||(myAge == myName.length)){
+        println("Both items are true")
+    }else{
+        //execute if false
+    }
+}
