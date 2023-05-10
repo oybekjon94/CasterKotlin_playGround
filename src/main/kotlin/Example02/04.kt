4f40de76e3ebbeea4631fa657b14dbe30f7e9667
oybekjon94 <x.oybek94@gmail.com>
2023-05-11 06:57:00 +0900

    [feat::] Add Example02

--- src/main/kotlin/Example02/04.kt
+++ src/main/kotlin/Example02/04.kt
@@ -3,6 +3,13 @@ package Example02
 fun main(){
     var x = sayHi()
     println(x)
+
+    var y = sayHi()
+    var d = sayHi()
+    var r = sayHi()
+    println(y)
+    println(d)
+    println(r)
 }
 fun sayHi(): String {
     return ("Hello World")
